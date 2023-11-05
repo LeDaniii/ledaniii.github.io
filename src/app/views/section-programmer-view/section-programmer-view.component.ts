bac82d4e83e2504edd9a620fea1f40fd907aa809
LeDaniii <danielmatzer@gmail.com>
2023-11-05 23:48:27 +0100

    added first background colors for each languages

--- src/app/views/section-programmer-view/section-programmer-view.component.ts
+++ src/app/views/section-programmer-view/section-programmer-view.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-section-programmer-view',
   templateUrl: './section-programmer-view.component.html',
-  styleUrls: ['./section-programmer-view.component.scss']
+  styleUrls: ['./section-programmer-view.component.scss'],
 })
 export class SectionProgrammerViewComponent {
 
+  mainHeaderClass: string = 'js-background';
+  headerIconClass: string = 'js';
+
+  chooseLanguage(language: string): void {
+    this.mainHeaderClass = `${language}-background`;
+    this.headerIconClass = language;
+  }
 }
